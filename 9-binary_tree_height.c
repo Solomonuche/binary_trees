1233f5bf884e9e48ba7caf4548e6bda0df285d3e
Solomon <solomonuche@gmail.com>
2023-11-29 19:10:13 +0100

    testing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,29 +4,38 @@
  * binary_tree_height - a function that measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to traverse
  *
- * Return: Length of node or NULL
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+
 	if (tree == NULL)
 		return (0);
-	return (height_helper(tree));
+	else
+		return (height_helper(tree));
 }
-/**
- * height_helper - a helper function that calculates the height of a tree
- * @tree: pointer to the root node of the tree
- *
- * Return: Length of the tree/ subtree
- */
+
 size_t height_helper(const binary_tree_t *tree)
 {
-	size_t lh, rh;
+	size_t ldepth, rdepth;
 
 	if (tree == NULL)
+	{
 		return (-1);
-	lh = height_helper(tree->left) + 1;
-	rh = height_helper(tree->right) + 1;
+	}
+	else
+	{
 
-	return ((lh > rh) ? lh : rh);
+		ldepth = height_helper(tree->left);
+		rdepth = height_helper(tree->right);
+
+		if (ldepth > rdepth)
+		{
+			return (rdepth + 1);
+		}
+		else
+		{
+			return (ldepth + 1);
+		}
+	}
 }
