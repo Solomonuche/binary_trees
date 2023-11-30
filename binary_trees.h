ff5b912048e88e0d2f1cf97441d4410c1d2821a6
Solomon <solomonuche@gmail.com>
2023-11-30 16:57:48 +0100

    updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -46,5 +46,6 @@ void traverse(const binary_tree_t *tree, int level, void (*func)(int));
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
