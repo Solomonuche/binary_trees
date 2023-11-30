f54e827268aa9328119e8b6c812e48be47bc7fff
Alex Sifuna <aleksifuna@gmail.com>
2023-11-30 10:44:11 +0000

    added a function that rotates a tree right

--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void traverse(const binary_tree_t *tree, int level, void (*func)(int));
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
