8b9f80432d7a535e87334686b54bd9d43b7f3af0
Alex Sifuna <aleksifuna@gmail.com>
2023-11-30 10:32:06 +0000

    added a function that rotates a tree left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,6 +9,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *new_tree;
 
+	if (tree == NULL)
+		return (NULL);
 	new_tree = tree->right;
 	tree->right = new_tree->left;
 	if (new_tree->left != NULL)
